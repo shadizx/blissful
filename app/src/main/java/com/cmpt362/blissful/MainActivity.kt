1bab6524e50799599cbd494971845349dc2c9428
Garry Chahal <gsc14@sfu.ca>
2023-11-08 20:06:12 -0800

    Use dark theme, Remove excess padding

--- app/src/main/java/com/cmpt362/blissful/MainActivity.kt
+++ app/src/main/java/com/cmpt362/blissful/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cmpt362.blissful
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set the theme to dark mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
