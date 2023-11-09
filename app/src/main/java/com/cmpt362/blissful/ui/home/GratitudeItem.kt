17e56a4b4154ca58b278029795df53c1c3fd31b2
Garry Chahal <gsc14@sfu.ca>
2023-11-09 09:58:39 -0800

    Update home page to show RecyclerView Layout with Images and Text

---
+++ app/src/main/java/com/cmpt362/blissful/ui/home/GratitudeItem.kt
@@ -0,0 +1,4 @@
+package com.cmpt362.blissful.ui.home
+
+data class GratitudeItem(val imageId: Int, val header: String, val description: String)
+
--- app/src/main/java/com/cmpt362/blissful/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cmpt362/blissful/ui/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.cmpt362.blissful.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cmpt362.blissful.R
 import com.cmpt362.blissful.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,22 +21,32 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var arrayList: ArrayList<GratitudeItem>
+    private lateinit var adapter: GratitudeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val recyclerView: RecyclerView = root.findViewById(R.id.home_gratitude_messages)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        arrayList = ArrayList()
+        adapter = GratitudeAdapter(arrayList)
+        recyclerView.adapter = adapter
+
+        homeViewModel.gratitudeItems.observe(viewLifecycleOwner) {
+            Log.d("HomeFragment", "Gratitude items changed: $it")
+            adapter.setData(it)
+            adapter.notifyDataSetChanged()
+            recyclerView.adapter = adapter
         }
+
         return root
     }
 
