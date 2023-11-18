7fdf9c50d72f546cfb012ef6e6853b2449a9fa3f
dya41 <dya41@sfu.ca>
2023-11-18 01:44:10 -0800

    Added database logic to register user and login user, now we can use onLoginSucceed() function to define futher behavior

--- app/src/main/java/com/cmpt362/blissful/ui/profile/RegisterActivity.kt
+++ app/src/main/java/com/cmpt362/blissful/ui/profile/RegisterActivity.kt
@@ -5,36 +5,71 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.cmpt362.blissful.R
+import com.cmpt362.blissful.db.User
+import com.cmpt362.blissful.db.UserDatabase
+import com.cmpt362.blissful.db.UserDatabaseDao
+import com.cmpt362.blissful.db.UserRepository
+import com.cmpt362.blissful.db.UserViewModel
+import com.cmpt362.blissful.db.UserViewModelFactory
 
 class RegisterActivity : AppCompatActivity() {
+
+    // DB instances
+    private lateinit var database: UserDatabase
+    private lateinit var databaseDao: UserDatabaseDao
+    private lateinit var repository: UserRepository
+    private lateinit var viewModelFactory: UserViewModelFactory
+    private lateinit var userViewModel: UserViewModel
 
     private lateinit var editTextUsername: EditText
     private lateinit var editTextPassword: EditText
     private lateinit var buttonRegister: Button
 
+    private lateinit var username: String
+    private lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Initialize DB instances
+        database = UserDatabase.getInstance(this)
+        databaseDao = database.userDatabaseDao
+        repository = UserRepository(databaseDao)
+        viewModelFactory = UserViewModelFactory(repository)
+        userViewModel = ViewModelProvider(this, viewModelFactory)[UserViewModel::class.java]
+
 
         editTextUsername = findViewById(R.id.editTextUsername)
         editTextPassword = findViewById(R.id.editTextPassword)
         buttonRegister = findViewById(R.id.buttonRegister)
 
         buttonRegister.setOnClickListener {
+            validateInput()
+        }
+
+    }
+
+    private fun validateInput() {
+        username = editTextUsername.text.toString()
+        password = editTextPassword.text.toString()
+
+        if (username.isNotEmpty() && password.isNotEmpty()) {
             registerUser()
+            Toast.makeText(this, "Register Successful", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
     private fun registerUser() {
-        val username = editTextUsername.text.toString()
-        val password = editTextPassword.text.toString()
+        // Create new user record
+        val newUserRecord = User(0, username, password)
 
-        // Here, add your logic to verify the credentials
-        if (username.isNotEmpty() && password.isNotEmpty()) {
-            Toast.makeText(this, "Register Successful", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show()
-        }
+        // Insert the new user by the view model, into the Room DB
+        userViewModel.insert(newUserRecord)
     }
 }
