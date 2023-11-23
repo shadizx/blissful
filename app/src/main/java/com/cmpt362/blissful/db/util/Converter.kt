f96f008d2e19f62cf9b276d3e91c48869f537e1a
dya41 <dya41@sfu.ca>
2023-11-22 22:38:07 -0800

    Added comments

--- app/src/main/java/com/cmpt362/blissful/db/util/Converter.kt
+++ app/src/main/java/com/cmpt362/blissful/db/util/Converter.kt
@@ -3,6 +3,11 @@ import androidx.room.TypeConverter
 import java.util.Calendar
 
 object Converters {
+
+    /**
+     * A type converter between Calendar Type (type we used in MyRuns3) and datestamp (time converted into milliseconds).
+     * The millisec time value can be obtained by (e.g. current time): ```System.currentTimeMillis()```
+     */
     @TypeConverter
     fun calendarToDatestamp(calendar: Calendar): Long = calendar.timeInMillis
 
