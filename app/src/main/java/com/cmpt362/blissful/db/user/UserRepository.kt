7889efdaa6eaa368812db30fa1ee6f6b0579b4f0
dya41 <dya41@sfu.ca>
2023-11-30 23:22:47 -0800

    Chore

--- app/src/main/java/com/cmpt362/blissful/db/user/UserRepository.kt
+++ app/src/main/java/com/cmpt362/blissful/db/user/UserRepository.kt
@@ -6,6 +6,12 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 
+/**
+ * Splitting repositories and DAOs for each table (e.g., User and Post) enhances the maintainability and clarity of the code.
+ * Each repository and DAO focuses on each table.
+ * This separation allows for more organized and manageable code, and also increases the scalability of the project.
+ * This approach is also considered a common best practices especially for large Android Project.
+ */
 class UserRepository(private val db: FirebaseFirestore) {
 
     val allUsers: Flow<List<User>> = flow {
