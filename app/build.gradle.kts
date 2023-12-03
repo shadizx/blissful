772ee8412d57888377d00e1d244c2fae763b8e51
Lakshay-sethi <58126894+Lakshay-sethi@users.noreply.github.com>
2023-12-02 17:49:32 -0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,11 +49,12 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("com.squareup.picasso:picasso:2.71828")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.squareup.picasso:picasso:2.71828")
+    
     // Room
     val roomVersion = "2.6.0"
 
