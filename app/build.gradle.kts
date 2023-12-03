633cf745653f36c47e353438c6d4a7ef2303bffd
Lakshay-sethi <58126894+Lakshay-sethi@users.noreply.github.com>
2023-12-02 17:44:37 -0800

    removing commented code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
+    implementation("com.squareup.picasso:picasso:2.71828")
     // Room
     val roomVersion = "2.6.0"
 
